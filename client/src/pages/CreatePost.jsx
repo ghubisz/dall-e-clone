1aced6ead34aa44c1ef82401a3bb1c92ba6c3443
ghubisz <gab.op.pl@op.pl>
2023-08-04 14:52:30 +0200

    create-post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Form, useNavigate } from 'react-router-dom'
 
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
@@ -16,6 +16,18 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSupriseMe = () => {
+    
+  }
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -24,6 +36,30 @@ const CreatePost = () => {
             </h1>
             <p className="mt-2 text-[#666e75] text-[14px] max-w[500px]">Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>
         </div>
+
+        <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-5">
+            <FormField 
+              labelName="Your name"
+              type="text"
+              name="name"
+              placeholder="John Doe"
+              value={form.name}
+              handleChange={handleChange}
+            />
+            <FormField 
+              labelName="Prompt"
+              type="text"
+              name="prompt"
+              placeholder="A plush toy robot sitting against a yellow wall"
+              value={form.prompt}
+              handleChange={handleChange}
+              isSupriseMe
+              handleSupriseMe={handleSupriseMe}
+            />
+
+          </div>
+        </form>
     </section>
   )
 }
